9962776a42df65ee7e5a21931594e64dbb2d5a4d
Rafi basha <rafibasha@rafimac-2.local>
2018-11-20 14:31:32 +0530

    Initial Commit

---
+++ xcode9new/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xcode9new
+//
+//  Created by Rafi basha on 20/11/18.
+//  Copyright © 2018 Rafi basha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
